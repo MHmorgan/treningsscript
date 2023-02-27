19f5bdb0e5f62539c71910c28fd78372a4f1d5da
Magnus Aa. Hirth <magnus.hirth@gmail.com>
2023-02-27 15:15:34 +0100

    Cleanup db.py

--- app/db.py
+++ app/db.py
@@ -29,36 +29,3 @@ def init_db():
     db = get_db()
     with current_app.open_resource('sql/schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-
-
-def get():
-    sqlite3.threadsafety = 2
-    db = sqlite3.connect(config.DATABASE)
-    db.row_factory = sqlite3.Row
-    return db
-
-
-@contextmanager
-def connect():
-    con = get()
-    try:
-        yield con
-    finally:
-        con.commit()
-        con.close()
-
-
-@contextmanager
-def cursor():
-    con = get()
-    try:
-        with con:
-            yield con.cursor()
-    finally:
-        con.close()
-
-
-def init():
-    db = get()
-    with files('sql').joinpath('schema.sql').open() as f:
-        db.executescript(f.read())
